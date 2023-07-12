61e888e294353f51d098ffd7d4526beeab6f1877
suv-1312 <96910024+suv-1312@users.noreply.github.com>
2023-07-13 01:10:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "http://localhost:5000";
+const baseURL = "https://my-share-backend.vercel.app";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
